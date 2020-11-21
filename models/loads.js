7aee44e073c285289600e76804e9ea5b0e8e440a
1 <kutsarb@gmail.com>
2020-11-21 14:02:38 -0500

    added driver and loads models and schema

---
+++ models/loads.js
@@ -0,0 +1,72 @@
+module.exports = function (sequelize, DataTypes) {
+    var Loads = sequelize.define("Loads", {
+        broker: {
+            type: DataTypes.String,
+            allowNull: false,
+            validate: {
+                len: [1, 50]
+            }
+        },
+        loadNum: {
+            type: DataTypes.String,
+            allowNull: false,
+            validate: {
+                len: [1, 75]
+            }
+        },
+        puAddress: {
+            type: DataTypes.String,
+            allowNull: false,
+            validate: {
+                len: [1, 100]
+            }
+        },
+        doAddress: {
+            type: DataTypes.String,
+            allowNull: false,
+            validate: {
+                len: [1, 100]
+            }
+        },
+        puDate: {
+            type: DataTypes.DATETIME ,
+            allowNull: false,
+        },
+        dueDate: {
+            type: DataTypes.DATETIME ,
+            allowNull: false
+        },
+        trailer: {
+            type: DataTypes.String,
+            allowNull: false,
+            validate: {
+                len: [1, 50]
+            }
+        },
+        // commodity: {
+        //     type: DataTypes.String,
+        //     allowNull: true
+        // },
+        // weight: {
+        //     type: DataTypes.Int,
+        //     allowNull: true
+        // },
+        // pallets:{
+        //     type:DataTypes.Int,
+        //     allowNull:true
+        // },
+
+        active: DataTypes.Boolean,
+
+        defaultValue: false
+
+    })
+    Loads.associate = function(models){
+        Loads.belongsTo(models.Driver, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+    return Loads
+}
