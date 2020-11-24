47a467f789bbf7df0d5647130425cb0ada1aa3fd
Matt Baber <mbaber1142@gmail.com>
2020-11-23 21:50:40 -0500

    day 2, finished majority of backend functionaility

---
+++ public/assets/js/index.js
@@ -0,0 +1,14 @@
+// Driver Data
+// .Name
+// .Cell
+// .Truck
+
+// Load Data
+// .Broker
+// .Load Number
+// .pick up add
+// .drop off add
+// .pick up date/time
+// .due date date/time
+// .trailer
+// .active boolean
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 // Dependencies
 var express = require("express");
+var exphbs = require("express-handlebars");
 
 // Sets up the Express App
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-// Requiring our models for syncing
 var db = require("./models");
+
+// Requiring our models for syncing
+// var db = require("./models");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -15,10 +18,13 @@ app.use(express.json());
 // Static directory
 app.use(express.static("public"));
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Routes
-require("./controllers/   Enter Here   ")(app);
-require("./controllers/   Enter Here   ")(app);
-require("./controllers/   Enter Here   ")(app);
+require("./controllers/htmlRoutes.js")(app);
+require("./controllers/apiLoadRoutes.js")(app);
+require("./controllers/apiLoadRoutes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
