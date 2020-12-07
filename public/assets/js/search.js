12a25d496f65b446232da4f41b9d09305e723de4
Matt Baber <mbaber1142@gmail.com>
2020-12-06 22:31:56 -0500

    date adjust and flag buttons

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -6,7 +6,18 @@ $(document).ready(function () {
         $.ajax(`/api/search/${where}/${query}`, {
             type: "GET",
         }).then(function (res) {
-            console.log(res)
+            
+            // hbsObject = {
+            //     load: res
+            // };
+            // console.log(hbsObject)
+            
+            // var template = $("#handlebarsTest");
+            // var renderSearch = Handlebars.compile(template);
+            // $("#searchBlockTest") = renderSearch(hbsObject);
+
+
+
             for (let j = 0; j < res.length; j++) {
 
                 var resultsList = $("#results-list")
@@ -19,12 +30,13 @@ $(document).ready(function () {
                 var dueDate = $("<p>")
                 var trailer = $("<p>")
                 var driverName = $("<p>")
-                var futureBtn = $("<input>")
-                var futureBtnLabel = $("<label>")
-                var enRouteBtn = $("<input>")
-                var enRouteLabel = $("<label>")
-                var deliveredBtn = $("<input>")
-                var deliveredLabel = $("<label>")
+                var buttonGroup = $("<div>")
+                var buttonLabel1 = $("<label>")
+                var buttonLabel2 = $("<label>")
+                var buttonLabel3 = $("<label>")
+                var buttonInput1 = $("<input type='radio' name='options' autocomplete='off'>")
+                var buttonInput2 = $("<input type='radio' name='options' autocomplete='off'>")
+                var buttonInput3 = $("<input type='radio' name='options' autocomplete='off'>")
 
                 broker.addClass("p-2 m-2 border rounded")
                 loadNum.addClass("p-2 m-2 border rounded")
@@ -34,6 +46,11 @@ $(document).ready(function () {
                 dueDate.addClass("p-2 m-2 border rounded")
                 trailer.addClass("p-2 m-2 border rounded")
                 driverName.addClass("p-2 m-2 border rounded")
+                buttonGroup.addClass("btn-group btn-group-toggle")
+                buttonGroup.attr("data-toggle", "buttons")
+                buttonLabel1.addClass("btn btn-danger")
+                buttonLabel2.addClass("btn btn-warning")
+                buttonLabel3.addClass("btn btn-success")
                 broker.text(res[j].broker);
                 loadNum.text(res[j].loadNum);
                 puAddress.text(res[j].puAddress);
@@ -43,59 +60,47 @@ $(document).ready(function () {
                 trailer.text(res[j].trailer);
                 driverName.text(res[j].Driver.driverName);
 
-                futureBtn.addClass("future");
-                futureBtn.attr("type","radio");
-                futureBtn.attr("value","future");
-                futureBtn.attr("name",`${res[j].loadNum}`);
-                futureBtnLabel.attr("for","future");
-                futureBtnLabel.text("Future");
+                
 
-                enRouteBtn.addClass("enRoute");
-                enRouteBtn.attr("type","radio");
-                enRouteBtn.attr("value","enRoute");
-                enRouteBtn.attr("name",`${res[j].loadNum}`);
-                enRouteLabel.attr("for","enRoute");
-                enRouteLabel.text("En Route");
-
-                deliveredBtn.addClass("delivered");
-                deliveredBtn.attr("type","radio");
-                deliveredBtn.attr("value","delivered");
-                deliveredBtn.attr("name",`${res[j].loadNum}`);
-                deliveredLabel.attr("for","delivered");
-                deliveredLabel.text("Delivered");
-
-                resultBox.addClass("p-2 m-2 border rounded");
-            
+                resultBox.addClass("p-2 m-2 border rounded contentCol");
                 resultsList.append(resultBox);
-
                 resultBox.append(broker);
-
                 resultBox.append(loadNum);
-
                 resultBox.append(puAddress);
-
                 resultBox.append(doAddress);
-
                 resultBox.append(puDate);
-
                 resultBox.append(dueDate);
-
                 resultBox.append(trailer);
-
                 resultBox.append(driverName);
+                resultBox.append(buttonGroup)
+                
+                for (i=1; i<4; i++) {
+                    
 
-                resultBox.append(enRouteBtn);
-
-                resultBox.append(enRouteLabel);
-
-                resultBox.append(futureBtn);
-
-                resultBox.append(futureBtnLabel);
-
-                resultBox.append(deliveredBtn);
-
-                resultBox.append(deliveredLabel);
-
+            
+                    switch (i) {
+                        case 1:
+                            buttonInput1.attr("id", `option${i}`)
+                            
+                            buttonGroup.append(buttonLabel1)
+                            buttonLabel1.append(buttonInput1)
+                            buttonInput1.text(" delivered ")
+                            break;
+                        case 2:
+                            buttonInput2.attr("id", `option${i}`)
+                            buttonInput2.text(" en route ")
+                            buttonGroup.append(buttonLabel2)
+                            buttonLabel2.append(buttonInput2)
+                             break;
+                        case 3:
+                            buttonInput3.attr("id", `option${i}`)
+                            buttonInput3.text(" look ahead ")
+                            buttonGroup.append(buttonLabel3)
+                            buttonLabel3.append(buttonInput3)
+                            break;
+                    }
+                    
+                };
 
                 
                 //     switch(res[j]){ 
@@ -132,6 +137,15 @@ $(document).ready(function () {
 
     });
 
+    $("input[name='ourRadio']:radio").change(function() {
+     let query = ($(this).attr("name"));
+     let select = ($(this).attr("value"));
+        console.log(query)
+        console.log(select)
+        console.log("hi")
+    });
+   
 
 
-})
+
+});
