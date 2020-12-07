847b762a6f6ee2277c4ca5bd76186412b10ff00f
1 <kutsarb@gmail.com>
2020-12-07 08:39:42 -0500

    changed styling on search page

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -10,23 +10,29 @@ $(document).ready(function () {
             for (let j = 0; j < res.length; j++) {
 
                 var resultsList = $("#results-list")
-                var resultBox = $("<div>")
-                var broker = $("<p>")
-                var loadNum = $("<p>")
-                var puAddress = $("<p>")
-                var doAddress = $("<p>")
-                var puDate = $("<p>")
-                var dueDate = $("<p>")
-                var trailer = $("<p>")
-                var driverName = $("<p>")
+                var resultBox = $("<tr>")
+                var emptyRow = $("<tr>")
+                var emptyCell = $("<td>")
+                var broker = $("<td>")
+                var loadNum = $("<td>")
+                var puAddress = $("<td>")
+                var doAddress = $("<td>")
+                var puDate = $("<td>")
+                var dueDate = $("<td>")
+                var trailer = $("<td>")
+                var driverName = $("<td>")
                 var futureBtn = $("<input>")
                 var futureBtnLabel = $("<label>")
                 var enRouteBtn = $("<input>")
                 var enRouteLabel = $("<label>")
                 var deliveredBtn = $("<input>")
                 var deliveredLabel = $("<label>")
+                var brokerLabel = $("<label>")
 
-                broker.addClass("p-2 m-2 border rounded")
+                brokerLabel.attr("for","broker")
+                broker.text("Broker:")
+                broker.addClass("p-2 m-2 border rounded bold")
+                broker.addClass("broker")
                 loadNum.addClass("p-2 m-2 border rounded")
                 puAddress.addClass("p-2 m-2 border rounded")
                 doAddress.addClass("p-2 m-2 border rounded")
@@ -34,37 +40,41 @@ $(document).ready(function () {
                 dueDate.addClass("p-2 m-2 border rounded")
                 trailer.addClass("p-2 m-2 border rounded")
                 driverName.addClass("p-2 m-2 border rounded")
-                broker.text(res[j].broker);
-                loadNum.text(res[j].loadNum);
-                puAddress.text(res[j].puAddress);
-                doAddress.text(res[j].doAddress);
-                puDate.text(res[j].puDate);
-                dueDate.text(res[j].dueDate);
-                trailer.text(res[j].trailer);
-                driverName.text(res[j].Driver.driverName);
+                broker.html("Broker: "+'<br>'+res[j].broker);
+                loadNum.html("Load #: "+'<br>'+res[j].loadNum);
+                puAddress.html("Pick Up: "+'<br>'+res[j].puAddress);
+                doAddress.html("Drop Off: "+'<br>'+res[j].doAddress);
+                puDate.html("Pick Up: "+'<br>'+res[j].puDate);
+                dueDate.html("Drop Off: "+'<br>'+res[j].dueDate);
+                trailer.html("Trailer: "+'<br>'+res[j].trailer);
+                driverName.html("Driver: "+'<br>'+res[j].Driver.driverName);
+                emptyRow.addClass("spacer")
 
-                futureBtn.addClass("future");
+                futureBtn.addClass("radio");
                 futureBtn.attr("type","radio");
                 futureBtn.attr("value","future");
                 futureBtn.attr("name",`${res[j].loadNum}`);
                 futureBtnLabel.attr("for","future");
-                futureBtnLabel.text("Future");
+                futureBtnLabel.html("Future");
+                futureBtnLabel.addClass("labels")
 
-                enRouteBtn.addClass("enRoute");
+                enRouteBtn.addClass("radio");
                 enRouteBtn.attr("type","radio");
                 enRouteBtn.attr("value","enRoute");
                 enRouteBtn.attr("name",`${res[j].loadNum}`);
                 enRouteLabel.attr("for","enRoute");
                 enRouteLabel.text("En Route");
+                enRouteLabel.addClass("labels")
 
-                deliveredBtn.addClass("delivered");
+                deliveredBtn.addClass("radio");
                 deliveredBtn.attr("type","radio");
                 deliveredBtn.attr("value","delivered");
                 deliveredBtn.attr("name",`${res[j].loadNum}`);
                 deliveredLabel.attr("for","delivered");
                 deliveredLabel.text("Delivered");
+                deliveredLabel.addClass("labels")
 
-                resultBox.addClass("p-2 m-2 border rounded");
+                resultBox.addClass("rows p-2 m-2 border rounded");
             
                 resultsList.append(resultBox);
 
@@ -96,8 +106,15 @@ $(document).ready(function () {
 
                 resultBox.append(deliveredLabel);
 
+                resultBox.append(brokerLabel);
 
+                resultsList.append(emptyRow)
+
+                emptyRow.append(emptyCell)
                 
+                emptyRow.append(emptyCell)
+
+                emptyRow.append(emptyCell)
                 //     switch(res[j]){ 
 
                 //         case moment().add(0, 'days'): 
